b524e0122e68bb0a3ec6d033f121013b2cffe4b1
Prodipta Ghosh <prodiptag@gmail.com>
2020-03-02 19:18:03 +0530

    add ec2 list

--- piggin/ec2/cli.py
+++ piggin/ec2/cli.py
@@ -14,14 +14,21 @@
 
 
 import click
+from piggin.ec2.ec2 import AwsEC2
+from piggin.utils.types import OSEnvAwsReset
 
 CONTEXT_SETTINGS = dict(ignore_unknown_options=True,
                         allow_extra_args=True,
                         token_normalize_func=lambda x: x.lower())
 
 @click.group()
+@click.option(
+    '--region',
+    '-r',
+    default='us-east-1',
+    help='AWS region.')
 @click.pass_context
-def ec2(ctx):
+def ec2(ctx, region):
     """
         piggin ec2 commands to interact with AWS ec2 resources.
         
@@ -33,7 +40,7 @@ def ec2(ctx):
             piggin ec2 rm [options]\n
             piggin ec2 update [options]\n
     """
-    pass
+    ctx.obj['region'] = region
 
 @ec2.command(context_settings=CONTEXT_SETTINGS)
 @click.option(
@@ -45,5 +52,33 @@ def ls(ctx, verbose):
     """
         list ec2 instances.
     """
-    click.echo(f'handling ec2 ls command {verbose}')
-    click.echo(f'{ctx.obj}')
+    access_key = ctx.obj['access_key']
+    secret_key = ctx.obj['secret_key']
+    profile_name = ctx.obj['profile_name']
+    region = ctx.obj['region']
+    count = 0
+    
+    with OSEnvAwsReset(access_key, secret_key):
+        ec2 = AwsEC2(access_key, secret_key, profile_name, region)
+        instances = ec2.ls()
+        
+        msg = f'{"instance id":20}'
+        msg = f'{"instance name":20}'
+        msg = msg + f'{"instance_type":12}'
+        msg = msg + f'{"key_name":20}'
+        msg = msg + f'{"private_ip_address":16}'
+        msg = msg + f'{"public_dns_name":25}'
+        print(msg)
+        print('_'*115)
+        for instance in instances:
+            msg = f'{instance.id:20}'
+            msg = f'{instance.tags[0].get("Value"):20}'
+            msg = msg + f'{instance.instance_type:12}'
+            msg = msg + f'{instance.key_name:20}'
+            msg = msg + f'{instance.private_ip_address:16}'
+            msg = msg + f'{instance.public_dns_name:25}'
+            print(msg)
+            count += 1
+    
+    print('_'*115)
+    print(f'total running instances {count}')
